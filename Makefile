768b1888b70967014c5ccf741d037d32eafed4d3
Jens Yuechao Liu <jensliu29@gmail.com>
2025-10-15 17:46:20 +0200

    update

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 # C compiler
 CC     =  icx
+# CC		= gcc
 OPT3 	= -g -O3
 OPTG0 	= -g -O0
 
@@ -47,18 +48,16 @@ ultraclean:
 	rm -fr $(TARGETS) logs *.sh.o* *.sh.e* *.o *.ppm .tareador_precomputed* *.prv *.pcf *.row dependency_graph* *.times.txt *.ps *.txt *.log TRACE.* set-0 logs
 
 gen-heat-omp: heat-omp
-	./heat-omp test.dat -a 0 -o test-heat-jacobi-seq.ppm && \
-	./heat-omp test.dat -a 1 -o test-heat-gauss-seq.ppm
-# 	diff test-heat-jacobi-seq.pmm heat-jacobi-seq.pmm && \
-# 	diff test-heat-gauss-seq.ppm heat-gauss-seq.pmm 
+	./heat-omp test.dat -a 0 -o heat-jacobi-omp.ppm && \
+	./heat-omp test.dat -a 1 -o heat-gauss-omp.ppm
 
 original-ppms: heat-seq
-	./heat-seq test.dat -a 0 -o heat-jacobi-seq.ppm && \
+# 	./heat-seq test.dat -a 0 -o heat-jacobi-seq.ppm && \
 	./heat-seq test.dat -a 1 -o heat-gauss-seq.ppm
 	
 cfg-heat-omp: heat-omp.c solver-omp.c misc.c
 	cd cfgs && \
+	rm * && \
 	gcc $(CFLAGS) $(OPT3) $(OPENMP) ../heat-omp.c ../solver-omp.c ../misc.c $(LFLAGS) -fdump-tree-optimized-graph && \
 	dot -Tpng a-heat-omp.c.254t.optimized.dot -o heat-cfg.png && \
-	dot -Tpng a-solver-omp.c.254t.optimized.dot -o solver-cfg.png && \
-	rm a*
+	dot -Tpng a-solver-omp.c.254t.optimized.dot -o solver-cfg.png
--- solver-omp.c
+++ solver-omp.c
@@ -1,122 +1,132 @@
 #include "omp.h"
 
-#define lowerb(id, p, n)  ( id * (n/p) + (id < (n%p) ? id : n%p) )
-#define numElem(id, p, n) ( (n/p) + (id < (n%p)) )
-#define upperb(id, p, n)  ( lowerb(id, p, n) + numElem(id, p, n) - 1 )
+#define lowerb(id, p, n) (id * (n / p) + (id < (n % p) ? id : n % p))
+#define numElem(id, p, n) ((n / p) + (id < (n % p)))
+#define upperb(id, p, n) (lowerb(id, p, n) + numElem(id, p, n) - 1)
 
-#define min(a, b) ( (a < b) ? a : b )
-#define max(a, b) ( (a > b) ? a : b )
+#define min(a, b) ((a < b) ? a : b)
+#define max(a, b) ((a > b) ? a : b)
 
 extern int userparam;
 
 // Function to copy one matrix into another
-void copy_mat (double *u, double *v, unsigned sizex, unsigned sizey) {
+void copy_mat(double *u, double *v, unsigned sizex, unsigned sizey) {
 
-    int nblocksi=1;
-    int nblocksj=1;
+  int nblocksi = 1;
+  int nblocksj = 1;
 
-    for (int blocki=0; blocki<nblocksi; ++blocki) {
-      int i_start = lowerb(blocki, nblocksi, sizex);
-      int i_end = upperb(blocki, nblocksi, sizex);
-      for (int blockj=0; blockj<nblocksj; ++blockj) {
-        int j_start = lowerb(blockj, nblocksj, sizey);
-        int j_end = upperb(blockj, nblocksj, sizey);
-        for (int i=max(1, i_start); i<=min(sizex-2, i_end); i++)
-          for (int j=max(1, j_start); j<=min(sizey-2, j_end); j++)
-            v[i*sizey+j] = u[i*sizey+j];
-      }
+  for (int blocki = 0; blocki < nblocksi; ++blocki) {
+    int i_start = lowerb(blocki, nblocksi, sizex);
+    int i_end = upperb(blocki, nblocksi, sizex);
+    for (int blockj = 0; blockj < nblocksj; ++blockj) {
+      int j_start = lowerb(blockj, nblocksj, sizey);
+      int j_end = upperb(blockj, nblocksj, sizey);
+      for (int i = max(1, i_start); i <= min(sizex - 2, i_end); i++)
+        for (int j = max(1, j_start); j <= min(sizey - 2, j_end); j++)
+          v[i * sizey + j] = u[i * sizey + j];
     }
+  }
 }
 
-double solve_gauss (double *u, unsigned sizex, unsigned sizey);
+double solve_gauss(double *u, unsigned sizex, unsigned sizey);
 
 // 2D-blocked solver: one iteration step
-double solve (double *u, double *unew, unsigned sizex, unsigned sizey) {
-    double tmp, diff, sum=0.0;
+double solve(double *u, double *unew, unsigned sizex, unsigned sizey) {
+  double tmp, diff, sum = 0.0;
 
-    if (u==unew) return solve_gauss(u,sizex,sizey);
+  if (u == unew)
+    return solve_gauss(u, sizex, sizey);
 
-    int nblocksi=omp_get_max_threads();
-    int nblocksj=1;
+  int nblocksi = omp_get_max_threads();
+  int nblocksj = 1;
 
-    #pragma omp parallel private(tmp, diff) reduction(+:sum) // complete data sharing constructs here
-    {
-      int blocki = omp_get_thread_num();
-      int i_start = lowerb(blocki, nblocksi, sizex);
-      int i_end = upperb(blocki, nblocksi, sizex);
-      for (int blockj=0; blockj<nblocksj; ++blockj) {
-        int j_start = lowerb(blockj, nblocksj, sizey);
-        int j_end = upperb(blockj, nblocksj, sizey);
-        for (int i=max(1, i_start); i<=min(sizex-2, i_end); i++) {
-          for (int j=max(1, j_start); j<=min(sizey-2, j_end); j++) {
-	        tmp = 0.25 * ( u[ i*sizey	   + (j-1) ] +  // left
-                           u[ i*sizey	   + (j+1) ] +  // right
-                           u[ (i-1)*sizey + j     ] +  // top
-                           u[ (i+1)*sizey + j     ] ); // bottom
-	        diff = tmp - u[i*sizey+ j];
-	        sum += diff * diff;
-	        unew[i*sizey+j] = tmp;
-          }
+#pragma omp parallel private(tmp, diff)                                        \
+    reduction(+ : sum) // complete data sharing constructs here
+  {
+    int blocki = omp_get_thread_num();
+    int i_start = lowerb(blocki, nblocksi, sizex);
+    int i_end = upperb(blocki, nblocksi, sizex);
+    for (int blockj = 0; blockj < nblocksj; ++blockj) {
+      int j_start = lowerb(blockj, nblocksj, sizey);
+      int j_end = upperb(blockj, nblocksj, sizey);
+      for (int i = max(1, i_start); i <= min(sizex - 2, i_end); i++) {
+        for (int j = max(1, j_start); j <= min(sizey - 2, j_end); j++) {
+          tmp = 0.25 * (u[i * sizey + (j - 1)] + // left
+                        u[i * sizey + (j + 1)] + // right
+                        u[(i - 1) * sizey + j] + // top
+                        u[(i + 1) * sizey + j]); // bottom
+          diff = tmp - u[i * sizey + j];
+          sum += diff * diff;
+          unew[i * sizey + j] = tmp;
         }
       }
     }
+  }
 
-    // #pragma omp parallel
-    // {
-    //   int blocki = omp_get_thread_num();
-    //   int i_start = lowerb(blocki, nblocksi, sizex);
-    //   int i_end = upperb(blocki, nblocksi, sizex);
-    //   for (int blockj=0; blockj<nblocksj; ++blockj) {
-    //     int j_start = lowerb(blockj, nblocksj, sizey);
-    //     int j_end = upperb(blockj, nblocksj, sizey);
-    //     for (int i=max(1, i_start); i<=min(sizex-2, i_end); i++) {
-    //       for (int j=max(1, j_start); j<=min(sizey-2, j_end); j++) {
-	  //         u[i*sizey+j] = unew[i*sizey+j];
-    //       }
-    //     }
-    //   }
-    // }
+  // #pragma omp parallel
+  // {
+  //   int blocki = omp_get_thread_num();
+  //   int i_start = lowerb(blocki, nblocksi, sizex);
+  //   int i_end = upperb(blocki, nblocksi, sizex);
+  //   for (int blockj=0; blockj<nblocksj; ++blockj) {
+  //     int j_start = lowerb(blockj, nblocksj, sizey);
+  //     int j_end = upperb(blockj, nblocksj, sizey);
+  //     for (int i=max(1, i_start); i<=min(sizex-2, i_end); i++) {
+  //       for (int j=max(1, j_start); j<=min(sizey-2, j_end); j++) {
+  //         u[i*sizey+j] = unew[i*sizey+j];
+  //       }
+  //     }
+  //   }
+  // }
 
-    return sum;
+  return sum;
 }
 
 // 2D-blocked solver: one iteration step
-double solve_gauss (double *u, unsigned sizex, unsigned sizey) {
-    double tmp, diff, sum=0.0;
+double solve_gauss(double *u, const unsigned sizex, const unsigned sizey) {
+  double tmp, diff, sum = 0.0;
 
-    int nblocksi=omp_get_max_threads();
-    int nblocksj=1;
+  int nblocksi = omp_get_max_threads();
+  int nblocksj = 1;
 
-  #pragma omp taskgroup // complete data sharing constructs here
+  #pragma taskgroup
+  // #pragma omp parallel
+  // #pragma omp single
   {
-    for (int blocki=0; blocki<nblocksi; ++blocki) {
+    for (int blocki = 0; blocki < nblocksi; ++blocki) {
       int i_start = lowerb(blocki, nblocksi, sizex);
       int i_end = upperb(blocki, nblocksi, sizex);
-      for (int blockj=0; blockj<nblocksj; ++blockj) {
+      for (int blockj = 0; blockj < nblocksj; ++blockj) {
         int j_start = lowerb(blockj, nblocksj, sizey);
         int j_end = upperb(blockj, nblocksj, sizey);
-
         #pragma omp task \
-                depend(in: u[(blocki-1)*nblocksj+blockj], u[blocki*nblocksj+(blockj-1)])\
-                depend(out: u[blocki*nblocksj+blockj]) \
-                private(tmp, diff) \
-                in_reduction(+:sum)
+                    depend(in : u[(blocki - 1) * nblocksj + blockj], u[blocki * nblocksj + (blockj - 1)]) \
+                    depend(out : u[blocki * nblocksj + blockj]) \
+                    private(tmp, diff)  \
+                    in_reduction(+ : sum)
         {
-          for (int i=max(1, i_start); i<=min(sizex-2, i_end); i++) {
-            for (int j=max(1, j_start); j<=min(sizey-2, j_end); j++) {
-              tmp = 0.25 * ( u[ i*sizey	   + (j-1) ] +  // left
-                           u[ i*sizey	   + (j+1) ] +  // right
-                           u[ (i-1)*sizey + j     ] +  // top
-                           u[ (i+1)*sizey + j     ] ); // bottom
-	            diff = tmp - u[i*sizey+ j];
-	            sum += diff * diff;
-	            u[i*sizey+j] = tmp;
+          for (int i = max(1, i_start); i <= min(sizex - 2, i_end); i++) {
+            for (int j = max(1, j_start); j <= min(sizey - 2, j_end); j++) {
+
+              // #pragma omp task \
+              //       depend(in : u[(i - 1) * sizey + j], u[i * sizey + (j - 1)]) \
+              //       depend(out : u[i * sizey + j]) \
+              //       private(tmp, diff)  \
+              //       in_reduction(+ : sum)
+              {
+                tmp = 0.25 * (u[i * sizey + (j - 1)] + // left
+                            u[i * sizey + (j + 1)] + // right
+                            u[(i - 1) * sizey + j] + // top
+                            u[(i + 1) * sizey + j]); // bottom
+                diff = tmp - u[i * sizey + j];
+                sum += diff * diff;
+                u[i * sizey + j] = tmp;
+              }
             }
           }
         }
       }
     }
   }
-
-    return sum;
+  return sum;
 }
